9912cf8352152080cd90870f73dc65812908932e
Mateus Macedo <105501644+Mateusfdsm1@users.noreply.github.com>
2024-08-19 09:55:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from "./Post"
+
+function App() {
+  return (
+  <div>
+    <Post 
+      author= "Diego Fernandes" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sit ad animi, totam fugit, doloribus libero ratione dolorum consequatur pariatur ipsam inventore accusantium perferendis facilis rerum commodi eius eveniet est!"
+    />
+
+    <Post 
+      author= "Mateus Macedo" 
+      content="Um novo post"
+    />
+  </div>  
+  )
+}
+
+export default App
+
+
