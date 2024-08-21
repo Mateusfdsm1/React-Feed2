025d8320817a7dccf8e18e839907001b94074aac
Mateus Macedo <105501644+Mateusfdsm1@users.noreply.github.com>
2024-08-21 11:53:23 -0300

    components and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
-
+import './styles.css'
 function App() {
   return (
   <div>
+    <Header />
     <Post 
       author= "Diego Fernandes" 
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sit ad animi, totam fugit, doloribus libero ratione dolorum consequatur pariatur ipsam inventore accusantium perferendis facilis rerum commodi eius eveniet est!"
